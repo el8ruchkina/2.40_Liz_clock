e6a81ea701cbab5dab8ad185c5db4e3baa64eb20
el8ruchkina <115945562+el8ruchkina@users.noreply.github.com>
2022-10-16 21:43:55 +0300

    Add files via upload

---
+++ 2.40_Liz/2.40_Liz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        int hr, mn, ang;
+        Console.WriteLine("Значение угла:");
+        ang = int.Parse(Console.ReadLine());
+        if (ang < 360 && ang >= 0)
+        {
+            mn = 2*(ang%30);
+            hr = (ang - (ang % 30)) / 30;
+            Console.WriteLine($"Часов полных: {hr}, минут: {mn}");
+        }
+    }
+}
